be47e520daddee6a6fac602b443d66b5a4b868aa
asuraiv <asuraiv7@gmail.com>
2020-03-16 00:29:49 +0900

    cascade 다시 추가. import 문 정리

--- src/main/java/com/asuraiv/practice/domain/commerce/entity/Order.java
+++ src/main/java/com/asuraiv/practice/domain/commerce/entity/Order.java
@@ -34,7 +34,7 @@ public class Order {
 	@Column(name = "order_id")
 	private Long id;
 
-	@ManyToOne
+	@ManyToOne(cascade = CascadeType.ALL)
 	@JoinColumn(name = "member_id")
 	private Member member;
 
